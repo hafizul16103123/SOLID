a7c8615698175218486ac50d1f1a433b384f58de
hafiz <islamhafizul158@gmail.com>
2022-04-13 07:00:48 +0600

    Dependency invesion principal practised

---
+++ src/dependency-inversion/app.js
@@ -0,0 +1,90 @@
+class Store{
+    constructor(paymentProsessor){
+        this.paymentProsessor = paymentProsessor;
+    }
+    purchaseBike(quantity){
+        this.paymentProsessor.pay(500 * quantity)
+    }
+    purchaseHelmet(quantity){
+        this.paymentProsessor.pay(200 * quantity)
+    }
+}
+
+// payment Prosessors ,wrapper Api  for real Api implementation
+class StripePaymentProcessor{
+    constructor(user){
+        this.stripe = new Stripe(user)
+    }
+    pay(amount){
+    console.log({1:this.user})
+        this.stripe.makePayment(amount)
+    }
+}
+class PaypalPaymentProcessor{
+    constructor(user){
+        this.user = user
+        this.paypal = new Paypal()
+    }
+    pay(amount){
+        this.paypal.makePayment(this.user,amount/85)
+    }
+}
+class BkashPaymentProcessor{
+    constructor(user,paymentAddress){
+        this.bkash = new Bkash(user,paymentAddress)
+    }
+    pay(amount){
+        this.bkash.makePayment(amount)
+    }
+}
+class NagodPaymentProcessor{
+    constructor(user,paymentAddress){
+        this.user= user
+        this.paymentAddress= paymentAddress
+        this.nagad = new Nagad()
+    }
+    pay(amount){
+        this.nagad.makePayment(this.user,this.paymentAddress,amount)
+    }
+    
+}
+
+// payment APIs
+
+class Paypal{
+    makePayment(user,amount){
+        console.log(`Paypal payment for user ${user} is ${amount}`)
+    }
+}
+class Stripe{
+    constructor(user){
+        this.user = user;
+    }
+    makePayment(amount){
+        console.log(`Stripe payment for user ${this.user} is ${amount}`)
+    }
+}
+
+class Bkash{
+    constructor(user,paymentAddress){
+        this.user = user;
+        this.paymentAddress = paymentAddress;
+    }
+    makePayment(amount){
+        console.log(`Bkash payment for user ${this.user} is ${amount} and payment address is ${this.paymentAddress}`)
+    }
+}
+class Nagad{
+    makePayment(user,paymentAddress,amount){
+        console.log(`Nagad payment for user ${user} is ${amount} and payment address is ${paymentAddress}`)
+    }
+}
+
+// create store
+/**
+ * Now If we decided to change payment method,then only we have to change Paymant Processor
+ * without touching our previous code
+ */
+ const store = new Store(new NagodPaymentProcessor('Piyash',"Dhaka"));
+ store.purchaseBike(2)
+ store.purchaseHelmet(2)
